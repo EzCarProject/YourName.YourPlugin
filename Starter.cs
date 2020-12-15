6ebec24e7a98468f2a732f7c7efdce486b8ccbba
Creaous <68230846+Creaous@users.noreply.github.com>
2020-12-15 19:04:31 +1030

    First Release

---
+++ Starter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ECMU.Interface;
+
+namespace YourName.YourPlugin
+{
+    public class Starter : PluginImplementerButton 
+    {
+        public string PluginName()
+        {
+            // This is for the plugin name.
+            return "Your Plugin";
+        }
+
+        public string PluginVersion()
+        {
+            // This is for the plugin version.
+            return "1.0.0.0";
+        }
+
+        public string PluginAuthor()
+        {
+            // This is the plugin author, normally your name or alias.
+            return "Your Name";
+        }
+
+        public string PluginDescription()
+        {
+            // This is for your plugin description.
+            return "Your description";
+        }
+
+        public Icon PluginIcon()
+        {
+            // Don't mess with this.
+            return Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location);
+        }
+
+        public void ButtonAdder(System.Windows.Forms.Button btn)
+        {
+            // You need this no matter what. It uses the function below.
+            btn.Click += button_Click;
+        }
+
+        public void button_Click(object sender, EventArgs e)
+        {
+            // This is for the button in the plugins section on settings.
+            System.Windows.Forms.MessageBox.Show("I am example plugin");
+            // Add a main form variable to show the form.
+            Form1 main = new Form1();
+            // Show the form using the variable.
+            main.Show();
+        }
+    }
+}
---
+++ TestForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YourName.YourPlugin
+{
+    public partial class Form1 : Form
+    {
+        // Reference the Starter class to receive info (name, author, version).
+        Starter plugininfo = new Starter();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            // Set the label to include the plugin name.
+            label1.Text = "This is a form from the " + plugininfo.PluginName() + " plugin!";
+        }
+    }
+}
